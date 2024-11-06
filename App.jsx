2065fb7acb82c1deaf8ab955af17a3497ac4c8df
Akshat tripathi <117847973+akshattripathi186@users.noreply.github.com>
2024-11-06 17:50:14 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Herosection from "./components/Herosection";
+import Navigation from "./components/Navigation";
+const App = () => {
+return(
+    <div>
+     <Navigation/>
+     <Herosection/>
+    </div>
+)
+};
+export default App
---
+++ Herosection.jsx
@@ -0,0 +1,35 @@
+
+const Herosection = () => {
+    return(
+        
+           <main className="hero">
+            <div className="hero-content">
+            <h1>YOUR FEET DESERVE THE BEST</h1>
+                <p>YOUR FEET DESERVE THE BEST AND 
+                    WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+                <div className="btn">
+                    <button>Shop</button>
+                    <button className="secondary">categories</button>
+                    </div>    
+                    <div className="shop">
+                        <p>Also Available On</p>
+                        <div className="photo">
+                            <img src="/images/amazon.png" alt="amazon" />
+                            <img src="/images/flipkart.png" alt="flip" />
+
+                        </div>
+                    </div>
+                    
+                    </div> 
+                
+                 
+            
+            
+            <div className="hero-image">
+                <img src="/images/shoe_image.png" alt="shoe" />
+            </div>
+           </main>
+        
+    );
+    };
+    export default Herosection
